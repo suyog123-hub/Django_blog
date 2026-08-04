4303fd4417b9c032a3583784a2caa9b5dd1071d4
suyog khadka <ksuyog697@gmail.com>
2026-08-04 13:06:31 +0545

    fixed csrf

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-only-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
