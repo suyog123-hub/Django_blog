f16304d7ae623e31f11fbf1060a1cc81585938fe
suyog khadka <ksuyog697@gmail.com>
2026-08-04 13:11:49 +0545

    Customize Jazzmin admin theme with branding

--- blog/settings.py
+++ blog/settings.py
@@ -126,3 +126,68 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Jazzmin admin theme configuration
+JAZZMIN_SETTINGS = {
+    "site_title": "MyBlog Admin",
+    "site_header": "MyBlog",
+    "site_brand": "MyBlog",
+    "site_logo_classes": "img-circle",
+    "welcome_sign": "Welcome to the MyBlog Admin Panel",
+    "copyright": "MyBlog",
+    "search_model": ["core.Blog", "auth.User"],
+    "user_avatar": None,
+    "topmenu_links": [
+        {"name": "Home", "url": "home", "new_window": False},
+        {"app": "core"},
+        {"model": "auth.User"},
+    ],
+    "show_sidebar": True,
+    "navigation_expanded": True,
+    "order_with_respect_to": ["core", "auth"],
+    "show_ui_builder": True,
+    "changeform_format": "horizontal_tabs",
+    "icons": {
+        "auth.Group": "fas fa-users",
+        "auth.User": "fas fa-user",
+        "core.Blog": "fas fa-blog",
+    },
+    "default_icon_parents": "fas fa-circle",
+    "default_icon_children": "fas fa-circle",
+    "related_modal_active": True,
+    "custom_css": None,
+    "custom_js": None,
+    "language_chooser": False,
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-primary",
+    "accent": "accent-primary",
+    "navbar": "navbar-white navbar-light",
+    "no_navbar_border": False,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "flatly",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success",
+    },
+}
