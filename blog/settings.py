d8c8d201985ded86d2bde10eb2b8b6e68b4bfe3e
suyog khadka <ksuyog697@gmail.com>
2026-08-04 12:55:47 +0545

    Clean up repo for GitHub: gitignore env/db/cache, secure settings via decouple

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d165y7q%gxm_s8bkky6axl%(9astid#n2ebiw)h2wn0!y+jwcn'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-only-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
